32df6ce425c0db74233067c71f9a7a183a2f2a8f
anusreev2006-netizen <anusreev2006@gmail.com>
2026-08-08 02:13:17 +0530

    DESIGH : Resume, Details, preview

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import { HiDocumentText } from "react-icons/hi2";
 import { HiDocumentDownload } from "react-icons/hi";
+import{Link} from 'react-router-dom'
+
 function Resume() {
   return (
-    <div style={{minHeight:'80vh'}} className='my-5'>
-      rtyuio
-
+    <div style={{minHeight:'90vh'}} className='mt-5'>
+      <h1 className='text-center'>Create an ATS Friendly Resume in Minutes with AI</h1>
+      <div container mt-5>
+        <div className='row'>
+          <div className='col-md-1 '></div>
+          <div className='col-md-4 rounded p-5 shadow text-center '>
+            <HiDocumentText className='fs-1 text-primary mb-3' />
+            <h4>Add Your Details  </h4>
+            <p>Our AI will generate Skills & Summary</p>
+            <h5>step 1</h5>
+          </div>
+          <div className='col-md-2'></div>
+          <div className='col-md-4 rounded p-5 shadow text-center'>
+            <HiDocumentDownload  className='fs-1 text-danger mb-3'/>
+            <h4>Download your Resume  </h4>
+            <p>Download PDF and start applying</p>
+            <h5>step 2</h5>
+          </div>
+          <div className='col-md-1'></div>
+        </div>
+        <div className='mt-5 text-center'>
+          <Link to={'/resume-details'} style={{backgroundColor:'#714a2f'}} className='btn text-light'>LET'S START</Link>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Preview from '../components/Preview'
+import { HiDocumentDownload } from "react-icons/hi";
+import Edit from '../components/Edit'
+import { IoDocumentText } from "react-icons/io5";
+import { IoMdRefresh } from "react-icons/io";
 
 function View() {
   return (
-    <div>View</div>
+    <div className='container my-5'>
+      <div className="col-lg-2"></div>
+      <div className="col-lg-8">
+        {/* navigation linka/icons */}
+        <div className="d-flex justify-content-center align-items-center">
+          {/* download */}
+          <button style={{color:'#714a2f'}} className='btn  me-2'><HiDocumentDownload  className='fs-1'/>Download CV</button>
+          {/* edit */}
+          <Edit/>
+          {/* all resumes */}
+          <Link to={'/all-resumes'} style={{color:'#714a2f'}} className='btn  me-2'><IoDocumentText className='fs-3' />All resumes</Link>
+          {/* download history */}
+           <Link to={'/downloads'} style={{color:'#714a2f'}} className='btn  me-2'><IoDocumentText className='fs-3' />Download History</Link>
+          {/* go back option */}
+          <Link to={'/resume-details'} style={{color:'#714a2f'}} className='btn  me-2'><IoMdRefresh className='fs-3' />Home</Link>
+        </div>
+        {/* preview */}
+        <div className="p-5">
+          <Preview/>
+        </div>
+      </div>
+      <div className="col-lg-2"></div>
+    </div>
   )
 }
 
